684d7814fb4d8489626345edad6f194376e6c1ed
Alessio <g.ale@hotmail.it>
2018-02-21 17:04:41 +0100

    Initial commit

---
+++ Index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8 lang="it" >
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>Cittadella Calcio</title>
+	<link rel="stylesheet" href="style/Theme.css">
+</head>
+<body>
+	<header class="header-style">
+	</header><!-- /header -->
+
+	<section class="asd-description">
+		<div class="asd-title">
+			<h1>AS Cittadella</h1>
+		</div>
+	</section>
+	<section class="">
+		<aside>
+			
+		</aside>	
+	</section>
+	<footer class="footer-style">
+		<hr>
+		<h6>
+			Via Ca’ Dai Pase 41/b
+			35013 Cittadella (PD)
+			Telefono: 0495972693 
+			Fax: 0499408469
+			Email: info@ascittadella.it
+			<br>
+			Allenatore: Roberto Venturato
+		</h6>
+		<hr>
+		
+	</footer>
+</body>
+</html>
